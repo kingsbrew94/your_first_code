4c2ed095da728057a599dab1e058e0acc39af6fd
kingsbrew94 <31999408+kingsbrew94@users.noreply.github.com>
2017-10-07 16:30:12 +0000

    Update main.c
    This is a simple program in C language that displays Hello Holberton 32 times

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
-// Create main function
+
 int main()
 {
-  int i;  // create integer variable for a the loop
-  // Run a for-loop for 32 times
-  for (i = 0; i < 10; i++)
+  int i;  
+ 
+  for (i = 0; i < 32; i++)
   {
-    // Whilst looping print Hello Holberton
+   
     printf("Hello Holberton\n");
-  } // End Loop
-  return 0; // Return zero
-} // End Program
+  } 
+  return 0; 
+} 
 
